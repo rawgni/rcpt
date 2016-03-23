49ca21751b5d7e3a7161f2c274c39912f541700b
Ingwar Wirjawan <ingwar.wirjawan@gmail.com>
2016-03-23 10:26:23 +0700

    initial data

---
+++ rcpt/rcpt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rcpt
+//
+//  Created by Ingwar Wirjawan on 23/3/16.
+//  Copyright © 2016 gts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
