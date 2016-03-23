2319edc9837d78afaee24610253bef95071d47fb
Ingwar Wirjawan <ingwar.wirjawan@gmail.com>
2016-03-23 17:14:02 +0700

    test

---
+++ rcpt/rcpt/ReceiptTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptTableViewCell.swift
+//  rcpt
+//
+//  Created by Ingwar Wirjawan on 23/3/16.
+//  Copyright © 2016 gts. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
